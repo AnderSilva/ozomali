c10a3f85ec98a164dcb41451f7d54df264a71898
Rubens de Andrade <rubensaz14@gmail.com>
2021-03-15 23:44:32 -0300

    feat(default screen): added basic component

---
+++ frontend/ozomali/src/app/pages/default-screen/default-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-default-screen',
+  templateUrl: './default-screen.component.html',
+  styleUrls: ['./default-screen.component.scss']
+})
+export class DefaultScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
