146ac36c805db2a279e047f69978b7c997552c95
Rubens de Andrade <rubensaz14@gmail.com>
2021-03-16 00:44:56 -0300

    feat(header component): added ripple effect

--- frontend/ozomali/src/app/components/components.module.ts
+++ frontend/ozomali/src/app/components/components.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderMenuComponent } from './header-menu/header-menu.component';
 import { RouterModule } from '@angular/router';
+import { MatRippleModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [HeaderMenuComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatRippleModule],
+  exports: [HeaderMenuComponent],
 })
 export class ComponentsModule {}
