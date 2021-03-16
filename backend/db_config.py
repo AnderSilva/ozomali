177edd9f745707bb3f9261489f9a6ec3cea4a837
Parizoto <anderson.parizoto@alunofaculdadeimpacta.com.br>
2021-03-15 23:40:03 -0300

    teste heroku

--- backend/db_config.py
+++ backend/db_config.py
@@ -1,17 +1,19 @@
 from app import app
-# from flask_sqlalchemy import SQLAlchemy
-from flaskext.mysql import MySQL
+
+from flask_sqlalchemy import SQLAlchemy
+# from flaskext.mysql import MySQL
 import os 
 from dotenv import load_dotenv
 load_dotenv()
 
-db = MySQL()
-# db = SQLAlchemy()
+# db = MySQL()
+db = SQLAlchemy()
 
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = os.getenv('MYSQL_DATABASE_USER')
-app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv('MYSQL_DATABASE_PASSWORD')
-app.config['MYSQL_DATABASE_DB'] = os.getenv('MYSQL_DATABASE_DB')
-app.config['MYSQL_DATABASE_HOST'] = os.getenv('MYSQL_DATABASE_HOST')
+# app.config['MYSQL_DATABASE_USER'] = os.getenv('MYSQL_DATABASE_USER')
+# app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv('MYSQL_DATABASE_PASSWORD')
+# app.config['MYSQL_DATABASE_DB'] = os.getenv('MYSQL_DATABASE_DB')
+# app.config['MYSQL_DATABASE_HOST'] = os.getenv('MYSQL_DATABASE_HOST')
+app.config['DATABASE_URL'] = os.getenv('DATABASE_URL')
 
 db.init_app(app)
