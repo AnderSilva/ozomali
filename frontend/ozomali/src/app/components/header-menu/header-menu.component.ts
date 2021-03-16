ed599594362ff67ce19ebec6c38289436b264b0b
Rubens de Andrade <rubensaz14@gmail.com>
2021-03-16 01:44:04 -0300

    style(overall): made screen use the whole outlet space inside the content

--- frontend/ozomali/src/app/components/header-menu/header-menu.component.ts
+++ frontend/ozomali/src/app/components/header-menu/header-menu.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header-menu',
   templateUrl: './header-menu.component.html',
-  styleUrls: ['./header-menu.component.scss']
+  styleUrls: ['./header-menu.component.scss'],
 })
 export class HeaderMenuComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  @Output() public shouldLogout: EventEmitter<void>;
+  constructor() {
+    this.shouldLogout = new EventEmitter();
   }
 
+  ngOnInit(): void {}
+
+  public logout(): void {
+    this.shouldLogout.emit();
+  }
 }
