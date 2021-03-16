dce1278a27dde0ed9739aa809a63fc117470ae43
Rubens de Andrade <rubensaz14@gmail.com>
2021-03-15 23:46:00 -0300

    feat(routing): added routing between screens utilizing header component

--- frontend/ozomali/src/app/app-routing.module.ts
+++ frontend/ozomali/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderMenuComponent } from './components/header-menu/header-menu.component';
+import { DefaultScreenComponent } from './pages/default-screen/default-screen.component';
+import { ProductScreenComponent } from './pages/product-screen/product-screen.component';
+import { VendorScreenComponent } from './pages/vendor-screen/vendor-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HeaderMenuComponent,
+    children: [{ path: 'vendors', component: VendorScreenComponent },
+      { path: 'products', component: ProductScreenComponent },
+      { path: '', component: DefaultScreenComponent }],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- frontend/ozomali/src/app/app.module.ts
+++ frontend/ozomali/src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ComponentsModule } from './components/components.module';
+import { DefaultScreenComponent } from './pages/default-screen/default-screen.component';
+import { VendorScreenComponent } from './pages/vendor-screen/vendor-screen.component';
+import { ProductScreenComponent } from './pages/product-screen/product-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultScreenComponent,
+    VendorScreenComponent,
+    ProductScreenComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
