69db68d88f437596688c2819c1e7d045c57867bc
Rubens de Andrade <rubensaz14@gmail.com>
2021-03-15 23:45:21 -0300

    feat(vendor screen): added basic component

---
+++ frontend/ozomali/src/app/pages/vendor-screen/vendor-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vendor-screen',
+  templateUrl: './vendor-screen.component.html',
+  styleUrls: ['./vendor-screen.component.scss']
+})
+export class VendorScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
