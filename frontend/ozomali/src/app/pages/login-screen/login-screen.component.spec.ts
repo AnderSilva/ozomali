959aa0c5a01f6f2343d9b37a290f2337b14d4a0d
Rubens de Andrade <rubensaz14@gmail.com>
2021-03-16 01:18:20 -0300

    feat(login): added basic authentication page

---
+++ frontend/ozomali/src/app/pages/login-screen/login-screen.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LoginScreenComponent } from './login-screen.component';
+
+describe('LoginScreenComponent', () => {
+  let component: LoginScreenComponent;
+  let fixture: ComponentFixture<LoginScreenComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LoginScreenComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoginScreenComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/ozomali/src/app/pages/login-screen/login-screen.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login-screen',
+  templateUrl: './login-screen.component.html',
+  styleUrls: ['./login-screen.component.scss'],
+})
+export class LoginScreenComponent implements OnInit {
+  public isAuthenticated: boolean = false;
+
+  public loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  ngOnInit(): void {}
+
+  login(): void {
+    if (this.loginForm.invalid) {
+      return
+    }
+
+    this.isAuthenticated = !this.isAuthenticated
+  }
+}
