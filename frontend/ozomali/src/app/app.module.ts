2c8cb1054823e50e6067579faf2e0af65e4d443d
sayuri1810 <sayuri1810@gmail.com>
2021-03-17 21:51:35 -0300

    merge com master

--- frontend/ozomali/src/app/app.module.ts
+++ frontend/ozomali/src/app/app.module.ts
@@ -1,20 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatRippleModule } from '@angular/material/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ComponentsModule } from './components/components.module';
+import { DefaultScreenComponent } from './pages/default-screen/default-screen.component';
+import { VendorScreenComponent } from './pages/vendor-screen/vendor-screen.component';
+import { ProductScreenComponent } from './pages/product-screen/product-screen.component';
+import { LoginScreenComponent } from './pages/login-screen/login-screen.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultScreenComponent,
+    VendorScreenComponent,
+    ProductScreenComponent,
+    LoginScreenComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ComponentsModule,
+    MatRippleModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatMenuModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
