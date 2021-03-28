fccf00ff463e932c8d2ddd229a42d2498d4de971
Parizoto <andersonsparizoto@gmail.com>
2021-03-28 14:11:02 -0300

    Testes do user model

--- backend/app/main/config.py
+++ backend/app/main/config.py
@@ -4,25 +4,25 @@ from dotenv import load_dotenv
 load_dotenv()
 
 postgresql_base = os.getenv('SQLALCHEMY_DATABASE_URI')
-# postgresql_base_dev = 'sqlite:///' + os.path.join(basedir, 'ozomali_test.db')    
 postgresql_base_dev = os.getenv('SQLALCHEMY_DATABASE_URI_DEV')
+postgresql_base_tst = 'sqlite:///' + os.path.join(basedir, 'ozomali_test.db')    
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'CODIGO_SECRETO_OZAMALI_NONE_ATIVAR')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'CODIGO_SECRETO_OZOMALI_NONE_ATIVAR')
     SRF_ENABLED = True    
 
 
 class DevelopmentConfig(Config):    
     SQLALCHEMY_DATABASE_URI = postgresql_base_dev
-    SECRET_KEY = 'CODIGO_SECRETO_OZAMAL_DEV_ATIVAR'
+    SECRET_KEY = 'CODIGO_SECRETO_OZOMALI_DEV_ATIVAR'
     DEBUG = True    
-    SQLALCHEMY_ECHO = True    
+    SQLALCHEMY_ECHO = False    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class TestingConfig(Config):
-    SQLALCHEMY_DATABASE_URI = postgresql_base_dev    
-    SECRET_KEY = 'CODIGO_SECRETO_OZAMAL_TESTE_ATIVAR'
+    SQLALCHEMY_DATABASE_URI = postgresql_base_tst
+    SECRET_KEY = 'CODIGO_SECRETO_OZOMALI_TESTE_ATIVAR'
     DEBUG = True
     TESTING = True
     PRESERVE_CONTEXT_ON_EXCEPTION = False
--- backend/app/test/test_user_model.py
+++ backend/app/test/test_user_model.py
@@ -2,14 +2,19 @@ import unittest
 
 import datetime
 
+from manage import app
 from app.main import db
 from app.main.model.user import User
 from app.test.base import BaseTestCase
 
 
 class TestUserModel(BaseTestCase):
+    def create_app(self):
+        app.config.from_object('app.main.config.TestingConfig')
+        return app
 
     def test_encode_auth_token(self):        
+        self.assertTrue(app.config['SECRET_KEY'] == 'CODIGO_SECRETO_OZOMALI_TESTE_ATIVAR')
         user = User(
             nome='nometeste',
             login='test1@test.com',
@@ -21,6 +26,7 @@ class TestUserModel(BaseTestCase):
         self.assertTrue(isinstance(auth_token.encode('utf8'), bytes))
 
     def test_decode_auth_token(self):
+        self.assertTrue(app.config['SECRET_KEY'] == 'CODIGO_SECRETO_OZOMALI_TESTE_ATIVAR')
         user = User(
             nome='nometeste2',
             login='test2@test.com',
@@ -29,8 +35,9 @@ class TestUserModel(BaseTestCase):
         db.session.add(user)
         db.session.commit()
         auth_token = User.encode_auth_token(user.id)        
-        self.assertTrue(isinstance(auth_token, bytes))
-        self.assertTrue(User.decode_auth_token(auth_token.decode("utf-8") ) == 1)
+        self.assertTrue(isinstance(auth_token.encode('utf8'), bytes))
+        print(User.decode_auth_token(auth_token.encode("utf-8") ) )
+        self.assertTrue(User.decode_auth_token(auth_token.encode("utf-8") ) == 1)
 
 
 if __name__ == '__main__':
