895beb47fa3bd6a488ecccce597c9f2796fb2c05
Rubens de Andrade <rubensaz14@gmail.com>
2021-04-29 02:42:15 -0300

    feat(vendor): implemented new search, search results, register and update

---
+++ frontend/ozomali/src/app/components/vendor/vendor-register/vendor-register.component.ts
@@ -0,0 +1,219 @@
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output, SimpleChanges } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { debounceTime, filter, switchMap, take, takeUntil } from 'rxjs/operators';
+import { vendor } from 'src/app/interfaces/vendor.interface';
+import { AddressService } from 'src/app/services/address/address.service';
+import { NotificationService } from 'src/app/services/notification/notification.service';
+import { VendorService } from 'src/app/services/vendor/vendor.service';
+
+@Component({
+  selector: 'app-vendor-register',
+  templateUrl: './vendor-register.component.html',
+  styleUrls: ['./vendor-register.component.scss'],
+})
+export class VendorRegisterComponent implements OnInit, OnDestroy {
+  private readonly unsubscribe$: Subject<void> = new Subject<void>();
+  public isAddressLoading: boolean = false;
+  public isRegisterLoading: boolean = false;
+  public vendorRegisterForm: FormGroup;
+  public vendorSearchForm: FormGroup;
+  @Input() public vendor: any;
+  @Input() public isSearch: boolean;
+  @Output() public results = new EventEmitter<any>();
+  @Output() public clearSearch = new EventEmitter<any>();
+
+  constructor(
+    private formBuider: FormBuilder,
+    private addressService: AddressService,
+    private vendorService: VendorService,
+    private notifications: NotificationService,
+  ) {
+    this.vendorRegisterForm = this.formBuider.group({
+      cnpj: ['', Validators.required],
+      nome: ['', Validators.required],
+      cep: ['', Validators.required],
+      logradouro: ['', Validators.required],
+      numero: ['', Validators.required],
+      complemento: [''],
+      bairro: ['', Validators.required],
+      cidade: ['', Validators.required],
+      estado: ['', Validators.required],
+
+      ativo: [''],
+      id: [{ value: '', disabled: true }],
+    });
+
+    this.vendorSearchForm = this.formBuider.group({
+      id: ['', Validators.required],
+    });
+  }
+
+  ngOnInit(): void {
+    if (this.isSearch) {
+      this.vendorRegisterForm.controls['cnpj'].disable();
+    }
+
+    this.vendorRegisterForm
+      .get('cep')
+      .valueChanges.pipe(
+        takeUntil(this.unsubscribe$),
+        filter(cep => cep && cep.length === 8),
+        debounceTime(300),
+        switchMap(cep => {
+          this.isAddressLoading = true;
+          return this.addressService.getAddress(cep);
+        }),
+      )
+      .subscribe(address => {
+        this.isAddressLoading = false;
+        this.setAddress(address);
+      });
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.vendor.currentValue !== changes.vendor.previousValue) {
+      this.setVendorForm(this.vendor);
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+  public setAddress(address: any): void {
+    this.vendorRegisterForm.get('logradouro').setValue(address.logradouro);
+    this.vendorRegisterForm.get('bairro').setValue(address.bairro);
+    this.vendorRegisterForm.get('cidade').setValue(address.localidade);
+    this.vendorRegisterForm.get('estado').setValue(address.uf);
+  }
+
+  public clearForm(): void {
+    this.clearSearch.emit();
+    this.vendorRegisterForm.reset();
+    this.vendor = undefined;
+  }
+
+  public registerVendor(): void {
+    if (this.vendorRegisterForm.invalid || this.isRegisterLoading) {
+      this.vendorRegisterForm.markAllAsTouched();
+      return;
+    }
+
+    const confirmationModal = this.notifications.confirmationModal(
+      'Você realmente quer salvar esse cadastro?',
+      'Sim',
+      'Não',
+    );
+
+    confirmationModal
+      .afterClosed()
+      .pipe(
+        filter(confirmation => confirmation === true),
+        switchMap(() => {
+          this.isRegisterLoading = true;
+          const formValues = this.vendorRegisterForm.getRawValue();
+
+          return this.vendorService.createVendor(formValues);
+        }),
+        take(1),
+      )
+      .subscribe(
+        response => {
+          this.vendorRegisterForm.reset();
+          this.isRegisterLoading = false;
+          this.notifications.feedbackModal(response);
+        },
+        response => {
+          this.isRegisterLoading = false;
+          this.notifications.feedbackModal(response);
+        },
+      );
+  }
+
+  public onSearchVendor(): void {
+    if (this.vendorSearchForm.invalid || this.isRegisterLoading) {
+      this.vendorSearchForm.markAllAsTouched();
+      return;
+    }
+
+    this.isRegisterLoading = true;
+    const id = this.vendorSearchForm.get('id').value;
+
+    this.vendorService
+      .getVendorById(id)
+      .pipe(take(1))
+      .subscribe(
+        vendors => {
+          this.isRegisterLoading = false;
+          this.results.emit(vendors);
+        },
+        response => {
+          this.isRegisterLoading = false;
+          this.notifications.feedbackModal(response);
+        },
+      );
+  }
+
+  public setVendorForm(vendor: any): void {
+    this.vendorRegisterForm.get('cnpj').setValue(vendor.cnpj);
+    this.vendorRegisterForm.get('nome').setValue(vendor.nome);
+    this.vendorRegisterForm.get('cep').setValue(vendor.cep);
+    this.vendorRegisterForm.get('logradouro').setValue(vendor.logradouro);
+    this.vendorRegisterForm.get('numero').setValue(vendor.numero);
+    this.vendorRegisterForm.get('complemento').setValue(vendor.complemento);
+    this.vendorRegisterForm.get('bairro').setValue(vendor.bairro);
+    this.vendorRegisterForm.get('cidade').setValue(vendor.cidade);
+    this.vendorRegisterForm.get('estado').setValue(vendor.estado);
+
+    this.vendorRegisterForm.get('id').setValue(vendor.id);
+    this.vendorRegisterForm.get('ativo').setValue(vendor.ativo);
+  }
+
+  public getVendors(): void {
+    if (this.isRegisterLoading) {
+      return;
+    }
+
+    this.isRegisterLoading = true;
+
+    this.vendorService
+      .getVendors()
+      .pipe(take(1))
+      .subscribe(
+        vendors => {
+          this.isRegisterLoading = false;
+          this.results.emit(vendors.data);
+        },
+        response => {
+          this.isRegisterLoading = false;
+          this.notifications.feedbackModal(response);
+        },
+      );
+  }
+
+  public updateVendor(): void {
+    if (this.vendorRegisterForm.invalid || this.isRegisterLoading) {
+      this.vendorRegisterForm.markAllAsTouched();
+      return;
+    }
+
+    this.isRegisterLoading = true;
+    let formValues = this.vendorRegisterForm.getRawValue();
+
+    this.vendorService
+      .updateVendor(this.vendor.id, formValues)
+      .pipe(take(1))
+      .subscribe(
+        response => {
+          this.isRegisterLoading = false;
+          this.notifications.feedbackModal(response);
+        },
+        response => {
+          this.isRegisterLoading = false;
+          this.notifications.feedbackModal(response);
+        },
+      );
+  }
+}
--- frontend/ozomali/src/app/pages/vendor-screen/vendor-screen.component.ts
+++ frontend/ozomali/src/app/pages/vendor-screen/vendor-screen.component.ts
@@ -6,7 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vendor-screen.component.scss'],
 })
 export class VendorScreenComponent implements OnInit {
+  public vendors: any;
+  public resultVendor: any;
+  public shouldSearch: boolean = true;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  public setVendors(vendors: any): void {
+    this.vendors = vendors;
+  }
+
+  public loadVendor(vendor: any): void {
+    this.resultVendor = vendor;
+    this.shouldSearch = false;
+    this.vendors = undefined;
+  }
+
+  public clearSearch(): void {
+    this.shouldSearch = true;
+  }
 }
