abda24f321eabb227cf247bc7b8665e2aa80b900
Rubens de Andrade <rubensaz14@gmail.com>
2021-04-29 02:28:25 -0300

    refactor(auth service): changed to new api

--- frontend/ozomali/src/app/services/auth/auth.service.ts
+++ frontend/ozomali/src/app/services/auth/auth.service.ts
@@ -9,14 +9,14 @@ export class AuthService {
   private readonly baseUrl: string;
 
   constructor(private http: HttpClient) {
-    this.baseUrl = 'https://ozomali.herokuapp.com';
+    this.baseUrl = 'https://ozomali-api.herokuapp.com/api/v1/auth';
   }
 
   public login(params: any): Observable<any> {
-    return this.http.post(`${this.baseUrl}/auth`, params);
+    return this.http.post(`${this.baseUrl}/login`, params);
   }
 
-  public createUser(params: any): Observable<any> {
-    return this.http.post(`${this.baseUrl}/users`, params);
-  }
+  // public createUser(params: any): Observable<any> {
+  //   return this.http.post(`${this.baseUrl}/users`, params);
+  // }
 }
