af7254222079355ca2b5a11c579109e4b55dfc59
Rubens de Andrade <rubensaz14@gmail.com>
2021-05-23 06:13:01 -0300

    feat(movings screen): added movings register and card components

--- frontend/ozomali/src/app/pages/movings-screen/movings-screen.component.ts
+++ frontend/ozomali/src/app/pages/movings-screen/movings-screen.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-movings-screen',
   templateUrl: './movings-screen.component.html',
-  styleUrls: ['./movings-screen.component.scss']
+  styleUrls: ['./movings-screen.component.scss'],
 })
 export class MovingsScreenComponent implements OnInit {
+  public movings: any;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public setMovings(movings: any): void {
+    this.movings = movings;
   }
-
 }
