a02922bcc0569c2070871edff03db7bbf856a748
Rubens de Andrade <rubensaz14@gmail.com>
2021-06-04 19:19:36 -0300

    feat(default screen): added easter egg to invalid user token

--- frontend/ozomali/src/app/pages/default-screen/default-screen.component.ts
+++ frontend/ozomali/src/app/pages/default-screen/default-screen.component.ts
@@ -1,7 +1,21 @@
 import { Component } from '@angular/core';
+import { UserService } from 'src/app/stores/user';
 @Component({
   selector: 'app-default-screen',
   templateUrl: './default-screen.component.html',
   styleUrls: ['./default-screen.component.scss'],
 })
-export class DefaultScreenComponent {}
+export class DefaultScreenComponent {
+  constructor(private userService: UserService) {}
+
+  invalidateToken() {
+    this.userService.updateAuthentication('tokenInvalido', {
+      exp: 1622931211,
+      iat: 1622844806,
+      uid: 20,
+      name: 'Rubens de Andrade',
+      login: 'bibull',
+      perfil: 'admin',
+    });
+  }
+}
