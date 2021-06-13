85028f18fd6cb8929e495cfc19e8abe7a2496362
Rubens de Andrade <rubensaz14@gmail.com>
2021-06-13 14:43:30 -0300

    feat(easter egg): now it works for any user

--- frontend/ozomali/src/app/pages/default-screen/default-screen.component.ts
+++ frontend/ozomali/src/app/pages/default-screen/default-screen.component.ts
@@ -1,21 +1,16 @@
 import { Component } from '@angular/core';
-import { UserService } from 'src/app/stores/user';
+import { UserQuery, UserService } from 'src/app/stores/user';
 @Component({
   selector: 'app-default-screen',
   templateUrl: './default-screen.component.html',
   styleUrls: ['./default-screen.component.scss'],
 })
 export class DefaultScreenComponent {
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private userQuery: UserQuery) {}
 
   invalidateToken(): void {
-    this.userService.updateAuthentication('tokenInvalido', {
-      exp: 1622931211,
-      iat: 1622844806,
-      uid: 20,
-      name: 'Rubens de Andrade',
-      login: 'bibull',
-      perfil: 'admin',
-    });
+    const currentUserInfo = this.userQuery.userInfo;
+
+    this.userService.updateAuthentication('tokenInvalido', currentUserInfo);
   }
 }
