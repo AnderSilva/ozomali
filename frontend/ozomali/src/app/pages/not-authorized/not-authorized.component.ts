b2d182ce249d40d28de4a6767c8eedcd86230257
Rubens de Andrade <rubensaz14@gmail.com>
2021-06-13 10:41:25 -0300

    feat(error pages): added return button

--- frontend/ozomali/src/app/pages/not-authorized/not-authorized.component.ts
+++ frontend/ozomali/src/app/pages/not-authorized/not-authorized.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-not-authorized',
   templateUrl: './not-authorized.component.html',
   styleUrls: ['./not-authorized.component.scss'],
 })
-export class NotAuthorizedComponent {}
+export class NotAuthorizedComponent {
+  constructor(private router: Router) {}
+  return(): void {
+    this.router.navigate(['']);
+  }
+}
