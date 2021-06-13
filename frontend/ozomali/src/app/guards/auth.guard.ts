6c52799e6f16efcb812e5f85d2a2def227488424
Rubens de Andrade <rubensaz14@gmail.com>
2021-06-13 14:31:24 -0300

    feat(guards): added canLoad auth guard

---
+++ frontend/ozomali/src/app/guards/auth.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { CanLoad, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { UserQuery } from '../stores/user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanLoad {
+  private readonly isAuthenticated$: Observable<boolean>;
+
+  constructor(private router: Router, private userQuery: UserQuery) {
+    this.isAuthenticated$ = this.userQuery.isAuthenticated$;
+  }
+
+  canLoad(): boolean {
+    let isAuthenticated: boolean = false;
+
+    this.isAuthenticated$.subscribe(authenticated => (isAuthenticated = authenticated));
+
+    if (!isAuthenticated) {
+      this.router.navigate(['/login']);
+    }
+
+    return isAuthenticated;
+  }
+}
